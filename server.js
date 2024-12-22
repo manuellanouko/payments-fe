d0868e3692a054a757e2c00f1d81b53486fc56df
Manuella Nouko <manuellanouko@gmail.com>
2024-12-22 11:30:14 -0500

    deploy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/payments-fe'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/payments-fe/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/payments-fe/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
